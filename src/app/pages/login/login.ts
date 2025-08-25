9957ca323c1e44b30605e0aac2690b527fa5a413
dmknpd <soupuabt@gmail.com>
2025-08-25 16:34:10 +0300

    login template

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  form: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
-  }
-
-  submit() {
-    console.log(this.form.value); // { email: '...', password: '...' }
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 }
