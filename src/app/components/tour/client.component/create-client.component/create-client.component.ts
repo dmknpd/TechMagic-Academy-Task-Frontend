d3eeb4b5138bebec73dc5423f54680c040206595
dmknpd <soupuabt@gmail.com>
2025-08-30 14:17:31 +0300

    create client component

--- src/app/components/tour/client.component/create-client.component/create-client.component.ts
+++ src/app/components/tour/client.component/create-client.component/create-client.component.ts
@@ -1,83 +1,96 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
 
-import { Client } from '../../../../types/client';
 import { ClientService } from '../../../../services/client.service';
+import { FormErrorsService } from '../../../../services/form-errors.service';
+import { Client } from '../../../../types/client';
 import { InputComponent } from '../../../input.component/input.component';
 
 @Component({
   selector: 'app-create-client',
+  standalone: true,
   imports: [
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatStepperModule,
     ReactiveFormsModule,
     InputComponent,
   ],
   templateUrl: './create-client.component.html',
-  styleUrl: './create-client.component.css',
+  styleUrls: ['./create-client.component.css'],
 })
 export class CreateClientComponent {
-  client = inject(ClientService);
-  router = inject(Router);
+  private client = inject(ClientService);
+  private router = inject(Router);
+  private formErrors = inject(FormErrorsService);
 
   globalError = signal<string | null>(null);
 
-  clientForm = new FormGroup({
+  clientFormName = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(12),
+    ]),
     lastName: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
       Validators.maxLength(12),
     ]),
-    firstName: new FormControl('', [
+    middleName: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
       Validators.maxLength(12),
     ]),
   });
 
-  private setServerErrors(errors: Record<string, string[]>) {
-    for (const field in errors) {
-      const control = this.clientForm.get(field);
-      if (control) {
-        control.setErrors({ serverError: errors[field] });
-      }
-    }
-  }
+  clientFormContacts = new FormGroup({
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^\d{12}$/)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
-  onLogin() {
-    if (this.clientForm.invalid) {
-      this.clientForm.markAllAsTouched();
+  clientFormAddress = new FormGroup({
+    country: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+  });
+
+  onCreate() {
+    const combinedForm = new FormGroup({
+      ...this.clientFormName.controls,
+      ...this.clientFormContacts.controls,
+      ...this.clientFormAddress.controls,
+    });
+
+    this.formErrors.clearFormErrors(combinedForm.controls);
+    this.globalError.set(null);
+
+    if (combinedForm.invalid) {
+      combinedForm.markAllAsTouched();
       return;
     }
 
-    const formValue = this.clientForm.value;
-    this.client.create(formValue as Client).subscribe({
+    const formValue = combinedForm.value as Client;
+
+    this.client.create(formValue).subscribe({
       next: (response) => {
         if (response.success) {
           this.globalError.set(null);
           // this.router.navigateByUrl('/');
-        } else {
-          if (response.errors) this.setServerErrors(response.errors);
-          if (response.message) this.globalError.set(response.message);
         }
       },
       error: (err) => {
-        console.error('error', err);
-
-        if (err.error) {
-          if (err.error.errors) {
-            this.setServerErrors(err.error.errors);
-          } else if (err.error.message) {
-            this.globalError.set(err.error.message);
-            return;
-          }
+        if (err.error.errors) {
+          this.formErrors.setFormErrors(combinedForm.controls, err.error.errors);
+        } else if (err.error.message) {
+          this.globalError.set(err.error.message);
         }
       },
     });
