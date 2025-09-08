38f6edaed2e1709195cd9faec3a4294b9787203e
dmknpd <soupuabt@gmail.com>
2025-09-08 16:15:09 +0300

    deleted excess imports

--- src/app/services/api-service/itinerary.service.ts
+++ src/app/services/api-service/itinerary.service.ts
@@ -1,9 +1,6 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
 import { environment } from '../../../environments/environment';
-import { ApiResponse } from '../../types/res';
 import { Itinerary, ItineraryFormData } from '../../types/itinerary';
 import { ApiService } from './api.service';
 
